4d38849b3ee83467c82a60827e6bce30e0ccaa44
Kati Frantz <frantzenterprises777@gmail.com>
2018-07-03 06:37:37 +0100

    add props to card component

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 Vue.component('card', {
+  props: ['title', 'content'],
   template: `
   <div class="card">
     <div class="card-body">
       <h3 class="card-title">
-        Vuejs fundamentals
+        {{ title }}
       </h3>
       <div class="card-text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores, sunt facilis omnis ipsum hic, temporibus commodi eius animi atque culpa iste molestias quaerat ipsa! Dolores dolore voluptas labore cum!
+        {{ content }}
       </div>
     </div>
   </div>
